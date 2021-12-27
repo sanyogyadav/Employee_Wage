4da6225f377c2337543377c8025cc0a82c72efd5
Sanyog Yadav <sanyogyadav52@gmail.com>
2021-12-28 01:33:36 +0530

    This is the final commit in UC6

--- Wagecomputation.java
+++ Wagecomputation.java
@@ -8,6 +8,8 @@ class Wagecomputation {
                         public static final int EMP_RATE_PER_HOUR = 20;
 			// creating one more constant according to UC5
 			public static final int NUM_OF_WORKING_DAYS = 20;
+			// creating one more constant for max hours
+			public static final int MAX_HRS_IN_MONTH = 100;
 
         public static void main(String[] args) {
 
@@ -16,17 +18,25 @@ class Wagecomputation {
                         int emphrs = 0;
                         int totalwage = 0;
 			int totalempwage = 0;
+			int totalworkingdays = 0;
+			int totalemphrs = 0;
+
+			while (totalemphrs <= MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS) {
+				totalworkingdays++;
                         //  now generating the random value to compare
-			// type casting the value of double to int here
-			for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			// for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
                         //we are  checking employee is present or absent.
+			// type casting the value of double to int here
 			int empcheck = (int)Math.floor(Math.random() * 10) % 3;
                         switch (empcheck) {
 
                                 case FULL_TIME:
                                 System.out.println("Employee is doing full time");
                                 emphrs = 8;
+
+				totalemphrs += emphrs;
                                 totalwage = emphrs * EMP_RATE_PER_HOUR;
+				System.out.println("Total employee wage : " + totalempwage);
 				totalempwage += totalwage;
                                 System.out.println("Emp Wage for full day is " + totalwage);
                                 break;
@@ -34,7 +44,9 @@ class Wagecomputation {
                                 case PART_TIME:
                                 System.out.println("Employee is doing part time");
                                 emphrs = 4;
+				totalemphrs += emphrs;
                                 totalwage = emphrs * EMP_RATE_PER_HOUR;
+				System.out.println("Total employee wage : " + totalempwage);
 				totalempwage += totalwage;
                                 System.out.println("Emp Wage for part time is " + totalwage);
 				break;
@@ -42,9 +54,9 @@ class Wagecomputation {
                                 // If nothing matches it will run the default part.
                                 default:
                                 System.out.println("Employee is absent");
-                        }  //switch
-		} //for
-			System.out.println("Total employee wage : " + totalempwage);
+                        	}  //switch
+			} // while
+	////for	}
         }
 }
 
