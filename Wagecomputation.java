4aedce00c9602d521955e86155e2b6735343316c
Sanyog Yadav <sanyogyadav52@gmail.com>
2021-12-27 23:15:02 +0530

    This is commit in UC4

--- Wagecomputation.java
+++ Wagecomputation.java
@@ -1,36 +1,42 @@
-class Wagecomputation {
+class WagecomputationUC4 {
+
+			// Creating constant to  compare it with random generated value
+                	public static final int FULL_TIME = 1;
+                        // Craeting another constant for part time value
+                        public static final int PART_TIME = 2;
+                        // Creating on  more constant for employee rate is fixed
+                        public static final int EMP_RATE_PER_HOUR = 20;
 
         public static void main(String[] args) {
 
                 System.out.println("Welcome to employee wage computation");
-
-                        // Creating constant to  compare it with random generated value
-                        int FULL_TIME = 1;
-			// Craeting another constant for part time value
-			int PART_TIME = 2;
-                        // Creating on  more constant for employee rate is fixed
-                        int EMP_RATE_PER_HOUR = 20;
                         // Creating Variables
                         int emphrs = 0;
                         int totalwage = 0;
                         //  now generating the random value to compare
+			// type casting the value of double to int here
                         double empcheck = Math.floor(Math.random() * 10) % 3;
                         // double used for random() as it generates decimal value.
                         //we are  checking employee is present or absent.
-                        if (empcheck == FULL_TIME)
-                              {  System.out.println("Employee is Present");
+                        switch ((int)empcheck) {
+
+                                case FULL_TIME:
+                                System.out.println("Employee is doing full time");
                                 emphrs = 8;
                                 totalwage = emphrs * EMP_RATE_PER_HOUR;
                                 System.out.println("Emp Wage for full day is " + totalwage);
-			}
-			else if (empcheck == PART_TIME)
-                              {
-			        System.out.println("Employee is doing part time");
+                                break;
+
+                                case PART_TIME:
+                                System.out.println("Employee is doing part time");
                                 emphrs = 4;
                                 totalwage = emphrs * EMP_RATE_PER_HOUR;
                                 System.out.println("Emp Wage for part time is " + totalwage);
-			}
-			else
-				System.out.println("Employee is absent");
-	}
+				break;
+
+                                // If nothing matches it will run the default part.
+                                default:
+                                System.out.println("Employee is absent");
+                        }
+        }
 }
